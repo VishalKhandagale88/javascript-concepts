5b3d99a2280bcc9d405f0bc19534f6702b97ece5
vishal88Kh <117203641+VishalKhandagale88@users.noreply.github.com>
2023-10-12 15:24:23 +0000

    added varibles concepts

---
+++ 01_basics/01_varibles.js
@@ -0,0 +1,15 @@
+const id = 101010
+let email = "vishal@gmail.com"
+var password ="12345"
+cityName = "Pune"
+
+//id =7; can't chage the const varibles
+
+email = "v@gmail.com"
+password="leo"
+cityName="Mumbai"
+console.table([id,email,password,cityName])
+/** 
+ * var is not used frequently because of isuue in block scope and functional scope
+ * 
+*/
