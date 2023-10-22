3be21fc18d2d168a815a49c2305d496aa6475646
vishal <vishal88kh@gmail.com>
2023-10-22 21:59:46 +0530

    added method to change the content in paragraph

--- first.js
+++ first.js
@@ -1,2 +1,9 @@
 // how to changing the content of an element
 let paraElement =  document.querySelector(".content");
+
+// changing the content by usning function
+function changeContent(){
+    paraElement.innerHTML = "hello this content got changed"
+    paraElement.style.color = "red";
+    paraElement.style.fontSize = "30px"
+}
