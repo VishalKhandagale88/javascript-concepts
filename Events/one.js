1cd4e139db61b4ba2f557489d3d1680300e2bcbc
vishal <vishal88kh@gmail.com>
2023-10-24 19:56:15 +0530

    target, parentNode,tagName learned

--- Events/one.js
+++ Events/one.js
@@ -1,10 +1,25 @@
 document.querySelector(".blog").addEventListener("click", function(event){
     console.log(event);
-    console.log("Blog is clicked");
+    // console.log("Blog is clicked");
     event.stopPropagation();
 },false)
 
 document.querySelector(".images").addEventListener("click",function(event){
     console.log("images clicked");
-    console.log(event);
+    // console.log(event);
 },false)
+
+
+// about target and parentNode and removing the parent node
+// 1. target
+// 2. parentNode
+// 3. tagName
+document.querySelector(".images").addEventListener("click",function(event){
+    console.log(event.target.parentNode);
+    console.log(event);
+    if(event.target.tagName==='IMG'){
+        let removeImage = event.target.parentNode;
+        removeImage.remove();
+    }
+
+})
