9c3c5afdcc299f7b706d164576cfbfd716448358
vishal <vishal88kh@gmail.com>
2023-10-24 19:41:26 +0530

    stopPropagation() method

--- Events/one.js
+++ Events/one.js
@@ -1,3 +1,10 @@
 document.querySelector(".blog").addEventListener("click", function(event){
     console.log(event);
-})
+    console.log("Blog is clicked");
+    event.stopPropagation();
+},false)
+
+document.querySelector(".images").addEventListener("click",function(event){
+    console.log("images clicked");
+    console.log(event);
+},false)
