02c2eaef401339809b1950062d3641f86c844642
vishal <vishal88kh@gmail.com>
2023-10-25 20:44:27 +0530

    added forEach method

---
+++ 02_ArrayMethods/arrayMethods.js
@@ -0,0 +1,12 @@
+// forEach() 
+const number = [1,2,3,4,5];
+number.forEach(consoleItem);
+
+function consoleItem(item,index,arr){
+    console.log("Item");
+    console.log(item);
+    console.log("Index");
+    console.log(index);
+    console.log("Array");
+    console.log(arr);
+}
