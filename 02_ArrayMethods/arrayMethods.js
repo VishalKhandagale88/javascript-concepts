333a6289241e391e01a398b49b13c2e314b72dbb
vishal <vishal88kh@gmail.com>
2023-10-26 20:08:57 +0530

    map funtion learning

--- 02_ArrayMethods/arrayMethods.js
+++ 02_ArrayMethods/arrayMethods.js
@@ -31,12 +31,43 @@ number.forEach((item,index,arr)=>{
 
 console.log("---------------------------------------------------------------------------------");
 
-
+// map function does not create new array infact it create a new array and will return so
+// there will be no changes in the current array
 console.log("map function");
 const n1 = [1,3,4,4,5,2,5,6];
-n1.map(double)
+let mapArray = n1.map(double)
 
 function double(value,index,arr){
     return value*2;
 }
-console.log(n1);
+console.log(mapArray);
+
+//
+
+const products = [
+    {
+        name:'laptop',
+        price:1000,
+        count : 5
+    },
+    {
+        name:'desktop',
+        price:5000,
+        count : 4
+    },
+    {
+        name:'phone',
+        price:2000,
+        count : 3
+    }
+]
+
+let productOfPrice=products.map(item => item.price * item.count)
+
+// how to add new attribute and value with the existing array
+let creatANewProce = products.map(item =>({
+    name: item.name,
+    totalPrice:item.count*item.price
+}))
+
+console.log(creatANewProce);
