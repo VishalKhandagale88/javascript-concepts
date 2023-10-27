a6324398a940f67a33df5b1da1c5173a3088fa49
vishal <vishal88kh@gmail.com>
2023-10-27 18:09:55 +0530

    added practise problems on map function

--- 02_ArrayMethods/practiseProblems.js
+++ 02_ArrayMethods/practiseProblems.js
@@ -16,3 +16,50 @@ let ans1 =arr.map((value,item,arr)=>{
 })
 console.log(ans1);
 console.log("---------------------------------------------------------------------------------");
+
+/*
+Convert to Uppercase:
+
+Input: ['apple', 'banana', 'cherry']
+
+Output: ['APPLE', 'BANANA', 'CHERRY'] 
+*/
+
+const str = ['apple', 'banana', 'cherry'];
+let strToUpperCase =  str.map((value,index,arr)=>{
+    return value.toUpperCase()
+})
+console.log(strToUpperCase);
+
+console.log("---------------------------------------------------------------------------------");
+
+/*
+    Calculate the Length:
+
+    Input: ['dog', 'cat', 'elephant']
+
+    Output: [3, 3, 8]
+*/ 
+
+const animalArray = ['dog', 'cat', 'elephant']
+const animalArrayLength = animalArray.map((value,index,arr)=>{
+    return value.length;
+})
+console.log(animalArrayLength);
+
+/*
+
+Extract First Names:
+
+Input: ['John Doe', 'Alice Smith', 'Bob Johnson']
+
+Output: ['John', 'Alice', 'Bob']
+*/
+
+const names = ['John Doe', 'Alice Smith', 'Bob Johnson'];
+const namesFirstLetter = names.map((value,index,arr)=>{
+    const firstName = value.split(' ');
+    return firstName[0];
+})
+
+console.log(namesFirstLetter);
