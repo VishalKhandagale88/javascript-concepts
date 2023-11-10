2fe7866956518daf267e4069ae38da703952ab0a
vishal <vishal88kh@gmail.com>
2023-11-10 23:20:34 +0530

    added information of data type

--- 01_basics/03_convertionOperation.js
+++ 01_basics/03_convertionOperation.js
@@ -13,3 +13,13 @@ console.log("num varible is of type : ", typeof num); // this will return NaN
 
 let num1 = Number(null);
 console.log("num1 is of type : ",num1); // this will give 0 as converted value
+// 7 and 8 video s content need to understand  from chai aur javascript
+
+// there are two types of data typs
+// 1 . Primitive this are call by value means we will get the value that is accosiated
+// with this data 
+// 7 primitive types : String, Number, Boolean , null , nudefined, symbol, BigInt
+// symbol is used to make value unique
+// 
+// 2 . Reference type (or) non primitive data types
+// Arrays , Object , functions
