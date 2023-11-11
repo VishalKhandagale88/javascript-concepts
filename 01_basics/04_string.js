bb1299f5f7ac8316e9102263dc54a4779b254e78
vishal <vishal88kh@gmail.com>
2023-11-11 19:50:53 +0530

    added string methods

--- 01_basics/04_string.js
+++ 01_basics/04_string.js
@@ -1,2 +1,12 @@
 //11 chai aur code
 // String
+const myName = "vishal";
+const age = 23;
+console.log(`name is: ${myName} and age is: ${age}`); 
+// String methods;
+console.log("String methods");
+console.log(myName[0]);
+console.log(myName.length);
+console.log(myName.toUpperCase());
+console.log(myName.charAt(2));
+console.log(myName.indexOf(i));
