08ca2b837018259a516fd378dff2165ed6431709
vishal <vishal88kh@gmail.com>
2023-11-13 23:40:35 +0530

    learned how to create new date object

---
+++ 01_basics/06_dates.js
@@ -0,0 +1,14 @@
+// Dates
+let date = new Date()
+console.log(date.toString());
+console.log(date.toDateString());
+console.log(date.toLocaleDateString());
+console.log(typeof date);
+// months start with 0 in java script
+let createdDate = new Date(2023,0,23);
+console.log(createdDate);
+console.log(createdDate.toDateString());
+
+let date1 = new Date("01-14-20223");
+console.log(date1.toDateString());
+
