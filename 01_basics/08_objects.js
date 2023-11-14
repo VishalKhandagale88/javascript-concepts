73877150c7ce24fff110db82f370a3658fec4244
vishal <vishal88kh@gmail.com>
2023-11-14 22:53:41 +0530

    added user object

--- 01_basics/08_objects.js
+++ 01_basics/08_objects.js
@@ -1,2 +1,11 @@
 // 16 chai aur code
-// singleton 
+// singleton --> if we create object with constructor then we will get a singleton
+
+
+
+// object literals
+
+const user = {
+    name:"vishal",
+    age:20
+}
