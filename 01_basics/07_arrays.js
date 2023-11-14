4c04376f659eff22504efaaebcbe566fa08abe3d
vishal <vishal88kh@gmail.com>
2023-11-14 19:32:23 +0530

    declaration of array

---
+++ 01_basics/07_arrays.js
@@ -0,0 +1,11 @@
+// 14 Arrays
+// array is object in js
+// objects are resizable we dont have to give size of the array while declaring
+ 
+const array = [0,1,2,3,4,5];
+const names = ["vishal","sam","mike","nikhil"];
+
+console.log(array);
+console.log(names);
+
+// Array methods
